e27b188d4ba319a8005044d43ceaa31be63970a3
Thomas Starling <tom@Toms-MacBook-Pro.local>
2016-11-10 12:45:17 +0000

    Added comments to task 9 & 10
    Added comments to both task 9 and 10, still need to update task 10 to
    return the values rather than the longest time

--- Coursework Solutions/Basic/Task 10/main.cpp
+++ Coursework Solutions/Basic/Task 10/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <array>
 #include <vector>
 #include <ctime>
 #include <stdlib.h>
@@ -7,28 +6,37 @@
 using namespace std;
 
 /*
-    Function header
+    The function checkSequence searches a sequence to check for the longest subsequence
+    or the longest sorted sequence. It does this by using a loop to iterate over the 
+    sequence and a comparison statment to check if the value of index i is more than
+    or equal to i - 1, if it is then you increment the count by 1. If no then if the 
+    variable count is more than tmp you store count in tmp and count will then be set to 
+    zero since the subsequence is no longer valid due to a number not been more than i.
+ 
+    We are then taking both variables count and tmp and comparing to see which one is 
+    holding the longest subsequence count. The algorithm complexity of this is O(n)
+    since we are iterating through the sequence.
 */
 
 void checkSequence(int *s, int l)
 {
     int  count = 0, tmp = 0;
-    for(int i = 0; i < l; i++)
+    for(int i = 0; i < l; i++)  //O(n)
     {
-        if(s[i] >= s[i - 1])
+        if(s[i] >= s[i - 1])    //if i is more than i - 1
         {
-            count++;
+            count++;            //Increment count
         }
         else
         {
-            if(count > tmp)
+            if(count > tmp)     //If count is more than tmp
             {
-                tmp = count;
+                tmp = count;    //Store the value of count in tmp
             }
-            count = 0;
+            count = 0;          //Reset the count
         }
     }
-    cout << "Longest Subdequence: " << ((count > tmp) ? count : tmp) << endl;
+    cout << "Longest Subsequence: " << ((count > tmp) ? count : tmp) << endl;   //Which is higher?
 }
 
 int main(int argc, const char * argv[])
@@ -38,11 +46,11 @@ int main(int argc, const char * argv[])
     cout << "Enter size of sequence: ";
     cin >> size;
     
-    int sequence[size];
+    int sequence[size];         //Create array and set it with the size of the user input
     
-    for(i = 0; i < size; i++)
+    for(i = 0; i < size; i++)   //O(n)
     {
-        cout << "Enter number at index " << i << ": ";
+        cout << "Enter number at index " << i << ": ";  //User can input numbers
         cin >> sequence[i];
     }
     
--- Coursework Solutions/Basic/Task 9/main.cpp
+++ Coursework Solutions/Basic/Task 9/main.cpp
@@ -5,12 +5,27 @@
 
 using namespace std;
 
+/*
+    The function shown below is a binary search algorithm, the binary search is a divide on conquer search 
+    in the means of it divides the sequence in half, if the value is higher then the middle value + 1 else - 1
+    repeat until the value has been found. However in this instance my searching algorithm has been adapted to
+    see if their are any values between the users input, same sturcture as the regular binary search algorithm 
+    however it sees if the middle value in the sequence is between the users input, if there are value/values 
+    then return true otherwise it will increment or decrement the start or end depeneding on whether the value
+    is higher or lower.
+ 
+    As well as the binary search algorithm I have made this program robust by making automatic generation of a
+    sequence then using bubble sort to sort it in descending order making it searchable by the binary search 
+    algorithm. The algorithm complexity of binary search is O(log n) this is becuase you are dividing the
+    sequence. The complexity of bubblesort is O(n^2) since there are two nested for loops.
+*/
+
 bool binarySearch(int *s, int &l, int &h)
 {
     //Iterative approach
-    int start = 0;
-    int end = 9;
-
+    int start = 0;  //Begining of the sequence
+    int end = 9;    //End of the sequence - 1 so 9 since the size of the sequence is 10,
+                    //if we compute 10 we are out of index
     if(l >= h)
     {
         cout << "Error - Low value larger than high" << endl;
@@ -20,13 +35,13 @@ bool binarySearch(int *s, int &l, int &h)
     //Binary search
     while(start <= end) //O(log(n))
     {
-        int mid = (start + end) / 2;
+        int mid = (start + end) / 2;    //Divide the sequence to get the middle value
 
-        if(s[mid] > l && s[mid] < h)
+        if(s[mid] > l && s[mid] < h)    //If middle is between the high and low
         {
             return true;
         }
-        else if(s[mid] < l)
+        else if(s[mid] < l) //If middle is less than low
         {
             end = mid - 1;
         }
@@ -40,7 +55,7 @@ bool binarySearch(int *s, int &l, int &h)
 
 int main(int argc, const char * argv[])
 {
-    int sequence[10];
+    int sequence[10];   //Array with the size of 10
     int low, high, i, j;
 
     srand((unsigned)time(NULL));    //Random each time never the same
@@ -48,7 +63,7 @@ int main(int argc, const char * argv[])
     cout << "Random Generated Sequence: " << endl;
 
     //Fill vector with random numbers
-    for(i = 1; i < 10; i++)
+    for(i = 1; i < 10; i++) //O(n)
     {
         sequence[i] = 1 + rand() % 100;
         cout << sequence[i] << ", ";
@@ -83,7 +98,7 @@ int main(int argc, const char * argv[])
     cin >> high >> low;
 
     bool isIn = binarySearch(sequence, low, high);
-    cout << "Is there a value between your inputs in the array?: " << ((isIn == 1) ? "True" : "False") << endl;
+    cout << "Is there a value between your inputs in the array?: " << ((isIn == 1) ? "True" : "False") << endl; //Use tennery statment to see if the variable is holding true or false
 
     return 0;
 }
