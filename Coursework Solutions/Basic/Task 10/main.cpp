0f27b99390f8f8e7a5760039f7a48f3033374a6c
Thomas Starling <starlint@uni.coventry.ac.uk>
2016-11-12 12:39:15 +0000

    Spelling mistakes in task 10

--- Coursework Solutions/Basic/Task 10/main.cpp
+++ Coursework Solutions/Basic/Task 10/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ask 9#include <iostream>
 #include <vector>
 #include <ctime>
 #include <stdlib.h>
@@ -7,17 +7,17 @@ using namespace std;
 
 /*
     The function checkSequence searches a sequence to check for the longest subsequence
-    or the longest sorted sequence. It does this by using a loop to iterate over the 
-    sequence and a comparison statment to check if the value of index i is more than
-    or equal to i - 1, if it is then you increment the count by 1. If no then if the 
-    variable count is more than tmp you store count in tmp and count will then be set to 
+    or the longest sorted sequence. It does this by using a loop to iterate over the
+    sequence and a comparison statement to check if the value of index i is more than
+    or equal to i - 1, if it is then you increment the count by 1. If no then if the
+    variable count is more than tmp you store count in tmp and count will then be set to
     zero since the subsequence is no longer valid due to a number not been more than i.
- 
-    The expected output is the longest subsequence, a tennery statment is used ot compare to see if the 
-    count or tmp is higher then returns the number it is holding. The parameters are the sequence 
+
+    The expected output is the longest subsequence, a tennery statement is used ot compare to see if the
+    count or tmp is higher then returns the number it is holding. The parameters are the sequence
     called by pointer so the data can be modified and the size of the sequence used for the loops.
- 
-    We are then taking both variables count and tmp and comparing to see which one is 
+
+    We are then taking both variables count and tmp and comparing to see which one is
     holding the longest subsequence count. The algorithm complexity of this is O(n)
     since we are iterating through the sequence.
 */
@@ -45,21 +45,21 @@ void checkSequence(int *s, int l)
 
 /*
     The main function is where the user can input the size of the sequence and then every number
-    at thier respected indexs, using a for loop to add them iterativly. After the values have been 
+    at their respected indexs, using a for loop to add them iterativly. After the values have been
     inputted the function is called where the sequence and the size is passed as a parameter.
- 
-    It has been enclosed in a try statment since it prevent the user from inputting bad charecters.
+
+    It has been enclosed in a try statement since it prevent the user from inputting bad characters.
 */
 
 int main(int argc, const char * argv[])
 {
     int size, i;
-    
+
     cout << "Enter size of sequence: ";
     cin >> size;
-    
+
     int sequence[size];         //Create array and set it with the size of the user input
-    
+
     try
     {
         for(i = 0; i < size; i++)   //O(n)
@@ -67,7 +67,7 @@ int main(int argc, const char * argv[])
             cout << "Enter number at index " << i << ": ";  //User can input numbers
             cin >> sequence[i];
         }
-        
+
         checkSequence(sequence, size);
     }
     catch()
