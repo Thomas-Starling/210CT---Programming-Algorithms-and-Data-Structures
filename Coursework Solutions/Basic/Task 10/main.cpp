2fe401d3a9d636ecbf981b60bdba9128908d68f2
Thomas Starling <starlint@uni.coventry.ac.uk>
2016-11-15 16:06:09 +0000

    Updated Task 10
    Updated Task 10, now fully working. Previous version only returned the
    count, this version returns the numbers

--- Coursework Solutions/Basic/Task 10/main.cpp
+++ Coursework Solutions/Basic/Task 10/main.cpp
@@ -1,4 +1,4 @@
-ask 9#include <iostream>
+#include <iostream>
 #include <vector>
 #include <ctime>
 #include <stdlib.h>
@@ -6,41 +6,44 @@ ask 9#include <iostream>
 using namespace std;
 
 /*
-    The function checkSequence searches a sequence to check for the longest subsequence
-    or the longest sorted sequence. It does this by using a loop to iterate over the
-    sequence and a comparison statement to check if the value of index i is more than
-    or equal to i - 1, if it is then you increment the count by 1. If no then if the
-    variable count is more than tmp you store count in tmp and count will then be set to
-    zero since the subsequence is no longer valid due to a number not been more than i.
-
-    The expected output is the longest subsequence, a tennery statement is used ot compare to see if the
-    count or tmp is higher then returns the number it is holding. The parameters are the sequence
-    called by pointer so the data can be modified and the size of the sequence used for the loops.
-
-    We are then taking both variables count and tmp and comparing to see which one is
-    holding the longest subsequence count. The algorithm complexity of this is O(n)
-    since we are iterating through the sequence.
+    Function header
 */
 
 void checkSequence(int *s, int l)
 {
-    int  count = 0, tmp = 0;
+    vector<int> count, tmp;
+
     for(int i = 0; i < l; i++)  //O(n)
     {
-        if(s[i] >= s[i - 1])    //if i is more than i - 1
+        if(i == 0 || s[i] >= s[i - 1])
         {
-            count++;            //Increment count
+            count.push_back(s[i]);
         }
-        else
+        else if(s[i] < s[i - 1])
         {
-            if(count > tmp)     //If count is more than tmp
+            if(count.size() > tmp.size())
             {
-                tmp = count;    //Store the value of count in tmp
+                tmp = count;
             }
-            count = 0;          //Reset the count
+            count.clear();
+            count.push_back(s[i]);
         }
     }
-    cout << "Longest Subsequence: " << ((count > tmp) ? count : tmp) << endl;   //Which is higher?
+
+    if(count.size() > tmp.size())
+    {
+        for(int j = 0; j < count.size(); j++)
+        {
+            cout << count[j] << ", ";
+        }
+    }
+    else
+    {
+        for(int k = 0; k < tmp.size(); k++)
+        {
+            cout << tmp[k] << ", ";
+        }
+    }
 }
 
 /*
@@ -60,20 +63,11 @@ int main(int argc, const char * argv[])
 
     int sequence[size];         //Create array and set it with the size of the user input
 
-    try
+    for(i = 0; i < size; i++)   //O(n)
     {
-        for(i = 0; i < size; i++)   //O(n)
-        {
-            cout << "Enter number at index " << i << ": ";  //User can input numbers
-            cin >> sequence[i];
-        }
-
-        checkSequence(sequence, size);
+        cout << "Enter number at index " << i << ": ";  //User can input numbers
+        cin >> sequence[i];
     }
-    catch()
-    {
-        //Something happened
-    }
-
+    checkSequence(sequence, size);
     return 0;
 }
