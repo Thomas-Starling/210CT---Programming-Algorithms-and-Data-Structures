d22539570f1614ebaae24df5e91b2a898309db87
Thomas Starling <tom@Toms-MacBook-Pro.local>
2016-11-17 13:06:39 +0000

    Added comments to task 10

--- Coursework Solutions/Basic/Task 10/main.cpp
+++ Coursework Solutions/Basic/Task 10/main.cpp
@@ -6,7 +6,40 @@
 using namespace std;
 
 /*
-    Function header
+    The function checkSequence returns the longest subsequence of numbers in order in acsending order,
+    for instance a sequence of 1, 2, 3, 4, 1. The longest subsequence is 1, 2, 3, 4, rather than return
+    a count of it, this function returns the actual numbers from the sequence. Using a for loop to
+    loop through the sequence then running a comarision on the index of i. The comparision comapres i to
+    its previous element seeing it i is larger than i - 1, if it is then it increments the counter and 
+    continues to loop, until i is not larger than i - 1 then another conditional statment executes to
+    see if i is less than i - 1. So if the element of i is 4 and the next is i, the conditional will 
+    execute code with another conditional seeing if the count size is larger than the size of tmp, if
+    it is then copy all elements from count into tmp. If the condition is not met then it will clear all
+    elements in the vector and push i into i ready to repeat the whole thing again. Example:
+ 
+    Sequence = 1, 2, 1
+    1st Iteration:
+    s[i] = 1
+    i = 0 so push into vector
+    vector1 now has the value at index 0 of 1
+ 
+    2nd Iteration:
+    i = 1
+    s[i] = 2, s[i - 1] = 1, 2 is more than 1 so push back into vector
+    vector1 now have the values 1, 2
+    
+    3rd Iteration:
+    i = 2
+    s[i] = 1, s[i - 1] = 2, 1 is less 1 so now.....
+    if vector1 is more than vector2 which is it then copy all elements from 1 into 2
+    vector1 now has only 1 element of the value 1
+    vector 2 now has the values 1, 2
+ 
+    no more elements of i so now print the largest one
+ 
+    The pre-condition is that you have an array of random elements, not necessarily in order or even any
+    elements in order, you will not get an output otherwise since no elements are found. The expected 
+    output is that the program will return the longest sorted sequence of numbers.
 */
 
 void checkSequence(int *s, int l)
@@ -15,31 +48,31 @@ void checkSequence(int *s, int l)
 
     for(int i = 0; i < l; i++)  //O(n)
     {
-        if(i == 0 || s[i] >= s[i - 1])
+        if(i == 0 || s[i] >= s[i - 1])  //Check if previous element is smaller than i
         {
             count.push_back(s[i]);
         }
-        else if(s[i] < s[i - 1])
+        else if(s[i] < s[i - 1])    //Check if i is less than previous element
         {
-            if(count.size() > tmp.size())
+            if(count.size() > tmp.size())   //Check between which vector is larger
             {
                 tmp = count;
             }
-            count.clear();
-            count.push_back(s[i]);
+            count.clear();  //Clear all elements in the vector
+            count.push_back(s[i]);  //Store the current value of i in the vector ready
         }
     }
 
     if(count.size() > tmp.size())
     {
-        for(int j = 0; j < count.size(); j++)
+        for(int j = 0; j < count.size(); j++)   //O(n) Print vector
         {
             cout << count[j] << ", ";
         }
     }
     else
     {
-        for(int k = 0; k < tmp.size(); k++)
+        for(int k = 0; k < tmp.size(); k++) //O(n) Print vector
         {
             cout << tmp[k] << ", ";
         }
