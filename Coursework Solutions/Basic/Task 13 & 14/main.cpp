621e4e698a85986cea2dc0c93c4a16b9082a1b74
Thomas Starling <tom@toms-mbp.home>
2016-11-26 13:56:40 +0000

    Updated Task 13
    Added Depth and Breadth traversal need to finish, don’t think it is
    giving the correct values

--- Coursework Solutions/Basic/Task 13 & 14/main.cpp
+++ Coursework Solutions/Basic/Task 13 & 14/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <array>
 #include <fstream>
+#include <stack>
+#include <queue>
 
 using namespace std;
 
@@ -38,7 +40,7 @@ public:
     void addEdge(int i, int j)
     {
         adjacencyMatrix[i - 1][j - 1] = 1;
-        adjacencyMatrix[j - 1][i - 1] = 1;
+        //adjacencyMatrix[j - 1][i - 1] = 1;
         cout << "Edge " << i << " to " << j << " Added!" << endl;
     }
 
@@ -78,20 +80,78 @@ public:
         cout << endl;
     }
 
-    void Breadth()
-    {
-        ofstream output;
-        output.open ("breadth.txt");
-        output << "";
-        output.close();
-    }
-
-    void Depth()
+    void Depth(int v)
     {
         ofstream output;
         output.open ("depth.txt");
-        output << "";
+        
+        bool visited[this->numOfVertex];
+        for(int i = 0; i < numOfVertex; i++)
+        {
+            visited[i] = 0;
+        }
+        
+        v -= 1;
+        
+        stack<int> stack;
+        visited[v] = 1;
+        stack.push(v);
+
+        while (!stack.empty())
+        {
+            v = stack.top();
+            cout << v + 1 << " ";
+            output << v + 1 << " ";
+            stack.pop();
+            
+            for (int i = 0; i < numOfVertex; ++i)
+            {
+                if (!visited[i])
+                {
+                    visited[i] = 1;
+                    stack.push(i);
+                }
+            }
+        }
         output.close();
+        cout << endl;
+    }
+    
+    void Breadth(int v)
+    {
+        ofstream output;
+        output.open ("depth.txt");
+        
+        bool visited[this->numOfVertex];
+        for(int i = 0; i < numOfVertex; i++)
+        {
+            visited[i] = 0;
+        }
+        
+        v -= 1;
+        
+        queue<int> queue;
+        visited[v] = true;
+        queue.push(v);
+        
+        while(!queue.empty())
+        {
+            v = queue.front();
+            cout << v + 1 << " ";
+            output << v + 1 << " ";
+            queue.pop();
+
+            for(int i = 0; i < numOfVertex; i++)
+            {
+                if(!visited[i])
+                {
+                    visited[i] = 1;
+                    queue.push(i);
+                }
+            }
+        }
+        output.close();
+        cout << endl;
     }
 
     ~Graph()
@@ -105,24 +165,24 @@ int main()
     Graph *g = new Graph(5);
 
     g->addEdge(1, 2);
+    g->addEdge(2, 4);
     g->addEdge(2, 3);
-    g->addEdge(4, 1);
-    g->addEdge(5, 2);
     g->printGraph();
 
     cout << endl;
 
     g->addVertex(6);
-    g->addEdge(6, 4);
+    g->addEdge(4, 5);
+    g->addEdge(5, 1);
+    g->addEdge(1, 6);
     g->printGraph();
-
-    cout << endl;
-
-    g->addVertex(7);
-    g->addEdge(7, 3);
+    
     g->printGraph();
 
     g->printConnections();
+    
+    g->Depth(1);
+    g->Breadth(3);
 
     delete g;
     return 0;
